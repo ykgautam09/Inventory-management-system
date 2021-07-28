b264b92c1a441171470fb5ea76eda953a52cdbe4
ykgautam09 <yateeshgautam1357@gmail.com>
2021-07-28 22:19:56 +0530

    add home route and populate table data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const connection=require('./../modules/dbConnection');
+const connection = require('./../modules/dbConnection');
 
-router.get('/',(req,res)=>{
-    return res.render('home');
+router.get('/', (req, res) => {
+    connection.connection.query("SELECT * FROM `book`",(err,result)=>{
+        if (err) throw err;
+        console.log(result[0]);
+        return res.render('home',{_title:'Inventory Management System',bookData:result});
+    })
+    // return res.render('home',{_title:'Inventory Management System'});
 })
 
-module.exports=router;
+module.exports = router;
